f82fdd9e8c7eafb3ebb376f2bc7c99fa8376e2bd
Dũng Đinh <dinhtuandung.1998@gmail.com>
2018-10-02 17:11:37 +0700

    update-code-new-products

--- Tigren/Popup/Block/Carousel/OwlCarousel.php
+++ Tigren/Popup/Block/Carousel/OwlCarousel.php
@@ -13,29 +13,58 @@ class OwlCarousel extends \Magento\Catalog\Block\Product\ListProduct {
     protected $_bestSellersCollection;
 
     protected $_catalogProductTypeConfigurable;
+
+    protected $__catalogProductVisibility;
     public function __construct(Context $context, PostHelper $postDataHelper, Resolver $layerResolver, CategoryRepositoryInterface $categoryRepository, Data $urlHelper,
                                 \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
                                 \Magento\Sales\Model\ResourceModel\Report\Bestsellers\CollectionFactory $bestSellersCollection,
                                 \Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable $catalogProductTypeConfigurable,
+                                \Magento\Catalog\Model\Product\Visibility $catalogProductVisibility,
                                 array $data = [])
     {
         $this->_productCollectionFactory = $productCollectionFactory;
         $this->_bestSellersCollection = $bestSellersCollection;
         $this->_catalogProductTypeConfigurable = $catalogProductTypeConfigurable;
+        $this->_catalogProductVisibility = $catalogProductVisibility;
         parent::__construct($context, $postDataHelper, $layerResolver, $categoryRepository, $urlHelper, $data);
     }
 
-    public function getNewProducts()
+    protected function _getNewProducts()
     {
+        $today = date('Y-m-d');
         $collection = $this->_productCollectionFactory->create();
         $collection->addAttributeToSelect('*');
-        $collection->addAttributeToFilter('type_id', array('eq', array('virtual', 'grouped', 'configurable')));
-        $collection->addAttributeToSort('entity_id', 'desc');
+        $collection->addAttributeToFilter(
+            'news_from_date',
+            [
+                'or' => [
+                    0 => ['date' => true, 'to' => $today],
+                    1 => ['is' => new \Zend_Db_Expr('null')],
+                ]
+            ])->addAttributeToFilter(
+            'news_to_date',
+            [
+                'or' => [
+                    0 => ['date' => true, 'from' => $today],
+                    1 => ['is' => new \Zend_Db_Expr('null')]
+                ]
+            ])
+            ->addAttributeToFilter(
+            [
+                ['attribute' => 'news_from_date', 'is' => new \Zend_Db_Expr('not null')],
+                ['attribute' => 'news_to_date', 'is' => new \Zend_Db_Expr('not null')]
+            ]
+        );
         $collection->setPageSize(12);
         return $collection;
     }
 
-    private function getBestSellersIds()
+    public function getLoadedNewProductsCollection()
+    {
+        return $this->_getNewProducts();
+    }
+
+    protected function _getBestSellersIds()
     {
         $collection = $this->_bestSellersCollection->create();
         $ids = [];
@@ -50,13 +79,29 @@ class OwlCarousel extends \Magento\Catalog\Block\Product\ListProduct {
         return $ids;
     }
 
-    public function getBestSellersProducts()
+    protected function _getBestSellersProducts()
     {
-        $ids = $this->getBestSellersIds();
-        $collection = $this->_productCollectionFactory->create();
-        $collection->addAttributeToSelect('*');
-        $collection->addAttributeToFilter('entity_id', ['in' => $ids]);
-        $collection->setPageSize(12);
+//        $ids = $this->_getBestSellersIds();
+//        $collection = $this->_productCollectionFactory->create();
+//        $collection->addIdFilter($ids)
+//                    ->addAttributeToSelect('*')
+//                    ->setPageSize(12);
+        $date = new \Zend_Date();
+        $today = $date->setDay(1)->getDate()->get('Y-MM-dd');
+        $fromDay = $date->subMonth(1)->getDate()->get('Y-MM-dd');
+        $collection = $this->_productCollectionFactory->create()->addAttributeToSelect('*')->setPageSize(12);
+        $collection->getSelect()->joinRight(
+            array('aggregation' => $collection->getResource()->getTable('sales_bestsellers_aggregated_monthly')),
+            "e.entity_id = aggregation.product_id AND aggregation.period BETWEEN '{$fromDay}' AND '{$today}'",
+            array('SUM(aggregation.qty_ordered) AS sold_quantity')
+        )
+        ->group('e.entity_id')
+        ->order(array('sold_quantity DESC', 'e.created_at'));
         return $collection;
+    }
+
+    public function getLoadedBestSellersProductCollection()
+    {
+        return $this->_getBestSellersProducts();
     }
 }
--- Tigren/Popup/view/frontend/web/js/productcarousel.js
+++ Tigren/Popup/view/frontend/web/js/productcarousel.js
@@ -1,25 +1,34 @@
 define(
     ['jquery', 'owlcarousel'],
     function($) {
-        $(function () {
-            $('.owl-carousel').owlCarousel({
-                items: 5,
-                autoplay: true,
-                loop: true,
-                autoplayTimeout: 6000,
-                autoplayHoverPause: true,
-                responsive:{
-                    0:{
-                        items:1
-                    },
-                    600:{
-                        items:3
-                    },
-                    1000:{
-                        items:5
+        'use strict';
+
+        $.widget('tigren.productCarousel', {
+            _create: function() {
+                this._carousel();
+            },
+
+            _carousel: function() {
+                this.element.owlCarousel({
+                    items: 5,
+                    autoplay: true,
+                    loop: true,
+                    autoplayTimeout: 6000,
+                    autoplayHoverPause: true,
+                    responsive:{
+                        0:{
+                            items:1
+                        },
+                        600:{
+                            items:3
+                        },
+                        1000:{
+                            items:5
+                        }
                     }
-                }
-            });
+                });
+            }
         });
+        return $.tigren.productCarousel;
     }
 );
