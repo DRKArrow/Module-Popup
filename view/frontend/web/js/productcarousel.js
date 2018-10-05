37086e53619fc2482042928cf115d81f853fa87a
Dũng Đinh <dinhtuandung.1998@gmail.com>
2018-10-05 13:52:29 +0700

    - Update swatch opt

--- view/frontend/web/js/productcarousel.js
+++ view/frontend/web/js/productcarousel.js
@@ -10,21 +10,20 @@ define(
 
             _carousel: function() {
                 this.element.owlCarousel({
-                    items: 5,
+                    items: 4,
                     autoplay: true,
-                    loop: true,
                     autoplayTimeout: 6000,
                     autoplayHoverPause: true,
                     responsive:{
                         0:{
                             items:1
                         },
-                        600:{
-                            items:3
+                        500: {
+                            items:2
                         },
                         1000:{
-                            items:5
-                        }
+                            items:4
+                        },
                     }
                 });
             }
